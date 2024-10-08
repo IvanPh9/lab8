f12c0463975fcc45b5c1b8c2c4c002d10190bf1f
g3rfo <rozhchenkoivan@gmail.com>
2024-10-08 18:57:09 +0300

    Ivan Rozhchenko(функція data_remove, ораганізація діалогу з користувачем)

--- main.py
+++ main.py
@@ -35,12 +35,43 @@ students_data = {'Іванов Іван': {'group': "КН-39.1", 'course': 2, 's
                  'Ніжна Ірина': {'group': "КН-39.1", 'course': 2, 'subjects_grades': {'Пайтон': 84, 'ЧМ': 97, 'ММДО': 98}}
                  }
 
-# Викликаємо функцію для додавання студента через введення з клавіатури
-add_student_from_input()
+# Виводимо інформацію про студентів(Рожченко Іван: зробив з дії функцію)
+def print_students_data():
+    print("\nІнформація про студентів:")
+    for student, data in students_data.items():
+        print(f"{student} (Група {data['group']}, Курс {data['course']}): {data['subjects_grades']}")
 
-# Виводимо інформацію про студентів
-print("\nІнформація про студентів:")
-for student, data in students_data.items():
-    print(f"{student} (Група {data['group']}, Курс {data['course']}): {data['subjects_grades']}")
+# Функція для видалення студента за прізвищем і ім'ям(Рожченко Іван)
+def data_remove(key):
+    if key in students_data:
+        students_data.pop(key)
+        print(f"{student} видалений/на зі списку студентів")
+    else:
+        print("<--Студента/ку не знайдено-->")
 
-# Наступний студент: додати функцію видалення по прізвищю та ім'ям, організувати діалог між користувачем та програмою
+# Наступний студент: додати функцію видалення по прізвищу та ім'ям, організувати діалог між користувачем та програмою(Рожченко Іван)
+
+# Діалог з користувачем(Рожченко Іван)
+number = 0
+while True:
+    if number == 0:
+        print("\n<1>Вивести список студентів"
+            "\n<2>Додати студента/ку"
+            "\n<3>Видалити студента/ку"
+            "\n<0>Завершити програму")
+        number += 1
+    answer = int(input("\nВиберіть дію: "))
+    if answer == 0:
+        break;
+    elif answer == 1:
+        print_students_data()
+    elif answer == 2:
+        add_student_from_input()
+    elif answer == 3:
+        student = str(input("Введіть прізвище і ім'я: "))
+        data_remove(student)
+    else:
+        print("Помилка введення")
+print("Програму завершено")
+
+# Наступний студент: додати функцію обрахунку середнього балу студента за прізвищем, додати відповідну дію до діалогу
